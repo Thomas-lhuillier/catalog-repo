6b32f7a595136f963eb3c58d95b98f19045efe73
Tho Mas <thomas.lhuillier@cycloid.io>
2020-03-17 10:58:50 +0000

    Update Service Catalog Terraform: jordanian-dinar-array-528

---
+++ jordanian-dinar-array-528/default/terraform/module-jordanian-dinar-array-528/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "projection"
+}
+
+"provider" "vault" {
+  "address" = "structure"
+
+  "token" = "Regional"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
