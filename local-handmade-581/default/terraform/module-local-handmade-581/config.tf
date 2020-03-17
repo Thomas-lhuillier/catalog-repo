017a58b4ebd4f85734127988e56df7cab7a94b83
Tho Mas <thomas.lhuillier@cycloid.io>
2020-03-17 09:17:19 +0000

    Update Service Catalog Terraform: local-handmade-581

---
+++ local-handmade-581/default/terraform/module-local-handmade-581/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "Advanced"
+}
+
+"provider" "vault" {
+  "address" = "interface"
+
+  "token" = "Bedfordshire"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
