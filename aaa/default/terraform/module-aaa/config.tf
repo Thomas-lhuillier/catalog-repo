c2c9b3a617a73b874af9cb6cdfa8c83e7d6508c9
Thomas lhuillier <thomas.lhuillier@cycloid.io>
2020-08-28 14:00:30 +0000

    Update Service Catalog Terraform: aaa

---
+++ aaa/default/terraform/module-aaa/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "aaa"
+}
+
+"provider" "vault" {
+  "address" = "aa"
+
+  "token" = "aaa"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
