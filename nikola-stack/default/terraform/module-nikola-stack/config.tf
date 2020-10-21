a2c0a81ec836dfca331a4b983fb4cefa2235ddd9
Nikola Savanovic <nikola.savanovic@cycloid.io>
2020-10-21 09:13:49 +0000

    Update Service Catalog Terraform: nikola-stack

---
+++ nikola-stack/default/terraform/module-nikola-stack/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "EU"
+}
+
+"provider" "vault" {
+  "address" = "test-1"
+
+  "token" = "test-1"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
