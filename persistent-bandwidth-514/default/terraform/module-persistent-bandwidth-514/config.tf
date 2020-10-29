bcbb70c0c83ca15f1e2c3bc481d71f24a49e1a53
tho mas <thomas.lhuillier@cycloid.io>
2020-10-29 11:17:43 +0000

    Update Service Catalog Terraform: persistent-bandwidth-514

---
+++ persistent-bandwidth-514/default/terraform/module-persistent-bandwidth-514/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "24/7"
+}
+
+"provider" "vault" {
+  "address" = "harness"
+
+  "token" = "Movies"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
