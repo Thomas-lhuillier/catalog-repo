2e4498a308be2484fa8b32627203beda0ef23857
Daniele Gazzelloni <daniele.gazzelloni@cycloid.io>
2021-06-15 11:16:31 +0000

    Update Service Catalog Terraform: to_be_deleted

--- to_be_deleted/default/terraform/module-to_be_deleted/config.tf
+++ to_be_deleted/default/terraform/module-to_be_deleted/config.tf
@@ -18,7 +18,19 @@
   "token" = "test"
 }
 
-"resource" "aws_api_gateway_method_response" "sdsd" {}
+"resource" "aws_api_gateway_method_response" "sdsd" {
+  "resource_id" = "sdsds"
+}
+
+"resource" "aws_instance" "i2" {
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "vpc_security_group_ids" = ["${aws_security_group.generated_sg_aws_instance_i2.id}"]
+}
 
 "resource" "aws_instance" "inst" {
   "tags" = {
@@ -26,6 +38,92 @@
 
     "env" = "${var.env}"
   }
+
+  "vpc_security_group_ids" = ["${aws_security_group.generated_sg_aws_instance_inst.id}"]
+}
+
+"resource" "aws_security_group" "generated_sg_aws_instance_i2" {
+  "name_prefix" = "generated_sg_aws_instance_i2"
+
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+}
+
+"resource" "aws_security_group" "generated_sg_aws_instance_inst" {
+  "name_prefix" = "generated_sg_aws_instance_inst"
+
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+}
+
+"resource" "aws_security_group_rule" "rule_egress_generated_5NgP2f@o0" {
+  "description" = "sdsd"
+
+  "from_port" = "22"
+
+  "protocol" = "ICMP"
+
+  "security_group_id" = "${aws_security_group.generated_sg_aws_instance_i2.id}"
+
+  "source_security_group_id" = "${aws_security_group.generated_sg_aws_instance_inst.id}"
+
+  "to_port" = "22"
+
+  "type" = "egress"
+}
+
+"resource" "aws_security_group_rule" "rule_egress_generated_sD0zOmO5n" {
+  "description" = "113"
+
+  "from_port" = "112"
+
+  "protocol" = "ICMP"
+
+  "security_group_id" = "${aws_security_group.generated_sg_aws_instance_i2.id}"
+
+  "source_security_group_id" = "${aws_security_group.generated_sg_aws_instance_inst.id}"
+
+  "to_port" = "112"
+
+  "type" = "egress"
+}
+
+"resource" "aws_security_group_rule" "rule_ingress_generated_5NgP2f@o0" {
+  "description" = "sdsd"
+
+  "from_port" = "22"
+
+  "protocol" = "ICMP"
+
+  "security_group_id" = "${aws_security_group.generated_sg_aws_instance_inst.id}"
+
+  "source_security_group_id" = "${aws_security_group.generated_sg_aws_instance_i2.id}"
+
+  "to_port" = "22"
+
+  "type" = "ingress"
+}
+
+"resource" "aws_security_group_rule" "rule_ingress_generated_sD0zOmO5n" {
+  "description" = "113"
+
+  "from_port" = "112"
+
+  "protocol" = "ICMP"
+
+  "security_group_id" = "${aws_security_group.generated_sg_aws_instance_inst.id}"
+
+  "source_security_group_id" = "${aws_security_group.generated_sg_aws_instance_i2.id}"
+
+  "to_port" = "112"
+
+  "type" = "ingress"
 }
 
 "variable" "env" {
